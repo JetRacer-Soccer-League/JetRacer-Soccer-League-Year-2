838f98e28a9bd16c239dda07d4c030f3c2aca12a
Ara Vardanyan <51611306+ara-vardanyan@users.noreply.github.com>
2023-04-19 11:46:05 -0700

    Add files via upload

---
+++ Unity Simulation/AgentSoccer.cs
@@ -0,0 +1,180 @@
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Policies;
+
+public enum Team
+{
+    Blue = 0,
+    Purple = 1
+}
+
+public class Agent : MonoBehaviour
+{
+    // Note that that the detectable tags are different for the blue and purple teams. The order is
+    // * ball
+    // * own goal
+    // * opposing goal
+    // * wall
+    // * own teammate
+    // * opposing player
+
+    public enum Position
+    {
+        Striker,
+        Goalie,
+        Generic
+    }
+
+    [HideInInspector]
+    public Team team;
+    float m_KickPower;
+    // The coefficient for the reward for colliding with a ball. Set using curriculum.
+    float m_BallTouch;
+    public Position position;
+
+    const float k_Power = 2000f;
+    float m_Existential;
+    float m_LateralSpeed;
+    float m_ForwardSpeed;
+
+    [Header("RC Car Parameters")]
+    public float maxSteeringAngle = 30.0f;
+    public float acceleration = 5000.0f;
+    private float currentSteeringAngle;
+
+
+    [HideInInspector]
+    public Rigidbody agentRb;
+    SoccerSettings m_SoccerSettings;
+    BehaviorParameters m_BehaviorParameters;
+    public Vector3 initialPos;
+    public float rotSign;
+
+
+
+
+    EnvironmentParameters m_ResetParams;
+
+    public override void Initialize()
+    {
+        SoccerEnvController envController = GetComponentInParent<SoccerEnvController>();
+        if (envController != null)
+        {
+            m_Existential = 1f / envController.MaxEnvironmentSteps;
+        }
+        else
+        {
+            m_Existential = 1f / MaxStep;
+        }
+
+        m_BehaviorParameters = gameObject.GetComponent<BehaviorParameters>();
+        if (m_BehaviorParameters.TeamId == (int)Team.Blue)
+        {
+            team = Team.Blue;
+            initialPos = new Vector3(transform.position.x - 5f, .5f, transform.position.z);
+            rotSign = 1f;
+        }
+        else
+        {
+            team = Team.Purple;
+            initialPos = new Vector3(transform.position.x + 5f, .5f, transform.position.z);
+            rotSign = -1f;
+        }
+        if (position == Position.Goalie)
+        {
+            m_LateralSpeed = 1.0f;
+            m_ForwardSpeed = 1.0f;
+        }
+        else if (position == Position.Striker)
+        {
+            m_LateralSpeed = 0.3f;
+            m_ForwardSpeed = 1.3f;
+        }
+        else
+        {
+            m_LateralSpeed = 0.3f;
+            m_ForwardSpeed = 1.0f;
+        }
+        m_SoccerSettings = FindObjectOfType<SoccerSettings>();
+        agentRb = GetComponent<Rigidbody>();
+        agentRb.maxAngularVelocity = 500;
+
+        m_ResetParams = Academy.Instance.EnvironmentParameters;
+    }
+
+
+    public override void OnActionReceived(ActionBuffers actionBuffers)
+    {
+
+        // ... (Keep the existing code for rewarding and OnCollisionEnter)
+        if (position == Position.Goalie)
+        {
+            // Existential bonus for Goalies.
+            AddReward(m_Existential);
+        }
+        else if (position == Position.Striker)
+        {
+            // Existential penalty for Strikers
+            AddReward(-m_Existential);
+        }
+
+        var a = 0;
+
+        var continuousActions = actionBuffers.ContinuousActions;
+        float throttle = Mathf.Clamp(continuousActions[0], -1, 1);
+        float steeringAngle = Mathf.Clamp(continuousActions[1], -maxSteeringAngle, maxSteeringAngle);
+
+        currentSteeringAngle = steeringAngle;
+
+        // Update agent movement and rotation based on throttle and steering angle
+        agentRb.AddForce(transform.forward * throttle * acceleration * Time.deltaTime, ForceMode.Force);
+        transform.Rotate(0, steeringAngle * Time.deltaTime, 0);
+
+    }
+
+    public void CollectObservations(VectorSensor sensor)
+    {
+        // Add agent's current speed as an observation
+        float speed = agentRb.velocity.magnitude;
+        sensor.AddObservation(speed);
+
+        // Add agent's current steering angle as an observation
+        sensor.AddObservation(currentSteeringAngle);
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        var continuousActionsOut = actionsOut.ContinuousActions;
+        continuousActionsOut[0] = Input.GetAxis("Vertical");
+        continuousActionsOut[1] = maxSteeringAngle * Input.GetAxis("Horizontal");
+    }
+
+    /// <summary>
+    /// Used to provide a "kick" to the ball.
+    /// </summary>
+    void OnCollisionEnter(Collision c)
+    {
+        var force = k_Power * m_KickPower;
+        if (position == Position.Goalie)
+        {
+            force = k_Power;
+        }
+        if (c.gameObject.CompareTag("ball"))
+        {
+            AddReward(.2f * m_BallTouch);
+            var dir = c.contacts[0].point - transform.position;
+            dir = dir.normalized;
+            c.gameObject.GetComponent<Rigidbody>().AddForce(dir * force);
+        }
+    }
+
+    public override void OnEpisodeBegin()
+    {
+        m_BallTouch = m_ResetParams.GetWithDefault("ball_touch", 0);
+    }
+
+
+
+
+}
